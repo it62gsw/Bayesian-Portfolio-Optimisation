d0a7da41afcf2667b5f76c73ceeedb602dac6529
it62gsw <it62imp@gmail.com>
2023-11-24 13:21:22 +0900

    config of jupyterlab and gitignore

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,2 @@
+c.ContentsManager.hide_globs = ['__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib', '*~', '*.nb.py']
+c.ContentsManager.default_jupytext_formats = "ipynb,.nb.py:percent"
